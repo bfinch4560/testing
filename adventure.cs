66a6a446d3c9fa72816b7865bf8496f7cf2f90c6
bfinch4560 <87163981+bfinch4560@users.noreply.github.com>
2021-07-10 18:19:04 -0500

    Add files via upload

---
+++ adventure.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+class adventure
+{
+    static void Main()
+    {
+        int health = 100;
+        int karma = 50;
+        int hunger = 70;
+        int cinnamonRollCount = 3;
+        bool cinnamonRollsLooted = false;
+        string location = "";
+        List<string> validLocations = new List<string> { "spawn", "kitchen" };
+
+        WriteLine("You awake in a cave. As you begin to rise, you notice a waft of something sweet hit your nose. It seems to be coming to your left, through a shallow entrance.");
+        Write('\n');
+        WriteLine("Do you follow your nose? (Yes/Y or No/N)");
+        string input = ReadLine();
+        /* list of valid inputs for the spawn area in the beginning */
+        List<string> validSpawnInput1 = new List<string> { "Yes", "yes", "Y", "y" };
+        List<string> validSpawnInput2 = new List<string> { "No", "no", "N", "n" };
+        if (validSpawnInput1.Contains(input))
+        {
+            location = "kitchen";
+            cinnamonRollCount = 3;
+            WriteLine("You duck through the entrance and are greeted with a small room that resembles a primitive kitchen. A small fire burns under a pot. No one seems to be around..." + '\n' +
+            "However you immediately eye what was taunting your nose just prior. A small group of innocent, fluffy and sweet cinnamon rolls sits atop a rock counter.  (@) (@) (@) (<-- cinnamon rolls)" + '\n' +
+            "No one seems to be around, and they are only a couple feet away.  What do you do?");
+            WriteLine("To grab a cinnamon roll type grab. Otherwise, type leave.");
+            string cinnamonRollInput = ReadLine();
+            List<string> validCinnamonRollInput = new List<string> { "grab", "leave" };
+            while (cinnamonRollInput == "grab")
+            {
+                if (cinnamonRollCount > 0)
+                {
+                    --cinnamonRollCount;
+                    hunger = hunger - 5;
+                    karma = karma - 10;
+                    cinnamonRollsLooted = true;
+                    WriteLine("You grab one cinnamon roll and quickly eat it. While it partially satiates you, you can't help but feel a little guilty. Number of cinnamon rolls left: {0}", cinnamonRollCount);
+                    WriteLine("***Hunger decreased by 5 to {0}. Karma decreased by 10 to {1}***", hunger, karma);
+                    WriteLine("What would you like to do now? Grab another or leave (grab/leave)");
+                    cinnamonRollInput = ReadLine();
+                }
+                else if (cinnamonRollCount == 0)
+                {
+                    WriteLine("There's no cinnamon rolls left. You took them all, little thieving piggy.");
+                    WriteLine("Well, no sense in hanging around to see if the owner comes back. Type leave to leave the kitchen");
+                    cinnamonRollInput = ReadLine();
+                }
+
+            }
+            while (cinnamonRollInput == "leave")
+            {
+                if(cinnamonRollCount == 3)
+                {
+                    WriteLine("It's probably best to not touch what isn't yours.");
+                    WriteLine("You exit the kitchen and return to the cave you spaw...er...found yourself in.");
+                    cinnamonRollInput = "";
+                    location = "spawn";
+                }
+                else if (cinnamonRollCount < 3)
+                {
+                    WriteLine("You and your sticky, thieving hands leave the crime scene and head back to the cave.");
+                    location = "spawn";
+                    cinnamonRollInput = "";
+                }
+            }
+        }
+        if (validSpawnInput2.Contains(input))
+        {
+            WriteLine("You were not deceived by the sweet scent. Surely going after something so easy would have led to your demise. However, you do notice your stomach grumble. It would be best to find food");
+        }
+        while(location == "spawn")
+        {
+            WriteLine("What would you like to do?");
+            if (cinnamonRollCount > 0)
+                WriteLine("If you would like to search the kitchen again, type kitchen");
+            else if (cinnamonRollCount == 0)
+                WriteLine("Type right to search the right portion of the cave; ");
+        }
+    }
+}
---
+++ methodReturnTesting.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+class Testing
+{   
+    static void Main()
+    {
+        int cinnamonRollCount = 3;
+        while (cinnamonRollCount > 0)
+        {
+            WriteLine("Type 1, 2 or 3 to grab that number of cinnamon roll(s), or type leave to leave the cinnamon rolls. Number of cinnamon rolls left: {0}", cinnamonRollCount);
+            int cinnamonRollsGrabbed = Convert.ToInt32(ReadLine());
+            cinnamonRollCount -= cinnamonRollsGrabbed;
+            List<int> validCinnamonGrabInput = new List<int> { 1, 2, 3 };
+            if (validCinnamonGrabInput.Contains(cinnamonRollsGrabbed))
+            {
+                string interactCinnamonRollReturn = interactCinnamonRoll(cinnamonRollsGrabbed, cinnamonRollCount);
+                WriteLine(interactCinnamonRollReturn);
+            }
+        }
+    }
+    private static string interactCinnamonRoll(int cinnamonRollsGrabbed, int cinnamonRollCount)
+    {
+        string returnString;
+        if(cinnamonRollsGrabbed == 3)
+        {
+            WriteLine("You grabbed all the cinnamon rolls. Hungry are we?");
+            returnString = "";
+            return returnString;
+        }
+        else if (cinnamonRollCount > 1)
+        {
+            // cinnamonRollCount -= cinnamonRollsGrabbed;
+            WriteLine("You grabbed {0} cinnamon roll(s), {1} remain.", cinnamonRollsGrabbed, cinnamonRollCount);
+            returnString = "";
+            return returnString;
+        }
+        else if (cinnamonRollCount == 1)
+        {
+            WriteLine("One more cinnamon roll left.");
+            returnString = "";
+            return returnString;
+        }
+        else if (cinnamonRollCount == 0)
+        {
+            returnString = "No more cinnamon rolls";
+            return returnString;
+        }
+        else 
+        return returnString = "something went wrong"; 
+    }
+}
